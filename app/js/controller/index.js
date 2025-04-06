366d23baa8360b9345c5e493f91148cc4b544bef
Bohdan <bohdan1trush@gmail.com>
2025-04-06 04:51:46 +0300

    06.04.2025 04:31

--- app/js/controller/index.js
+++ app/js/controller/index.js
@@ -1,48 +1,41 @@
+import {
+  initIntervalSlider,
+  initFacilityGrid,
+  initSplideSliders,
+  initModal,
+  initSendForm,
+} from "../model/index.js";
 
 import {
-	initIntervalSlider,
-	initFacilityGrid,
-	initSplideSliders,
-	initModal,
-	initSendForm
-} from '../model/index.js';
-
-
-
-import {
-	// $accordeons,
-	$intervalSlider,
-	$facilityGrid,
-	$form
-	} from '../view/index.js';
-
+  // $accordeons,
+  $intervalSlider,
+  $facilityGrid,
+  $form,
+} from "../view/index.js";
 
 const app = {
-	init() {
-		this.iis();
-		this.ifg();
-		this.iss();
-		this.im();
-		this.isf();
-	},
-	iis(){
-		initIntervalSlider($intervalSlider);
-	},
-	ifg(){
-		initFacilityGrid($facilityGrid);
-	},
-	iss(){
-		initSplideSliders();
-	},
-	im(){
-		initModal();
-	},
-	isf(){
-		initSendForm($form);
-	}
-}
+  init() {
+    this.iis();
+    this.ifg();
+    this.iss();
+    this.im();
+    this.isf();
+  },
+  iis() {
+    initIntervalSlider($intervalSlider);
+  },
+  ifg() {
+    initFacilityGrid($facilityGrid);
+  },
+  iss() {
+    initSplideSliders();
+  },
+  im() {
+    initModal();
+  },
+  isf() {
+    initSendForm($form);
+  },
+};
 
-
-export {
-	app
-}
+export { app };
--- app/libs/libs.js
+++ app/libs/libs.js
@@ -1,19 +1,12 @@
-
 // import "bootstrap/js/dist/modal.js";
 // import jquery from "jquery";
 
 import Isotope from "isotope-layout";
-import Splide from '@splidejs/splide';
-import Micromodal from 'micromodal';
-import {Email} from './smtp.js'
+import Splide from "@splidejs/splide";
+import Micromodal from "micromodal";
+import { Email } from "./smtp.js";
 
 // window.$ = jquery;
 // window.jQuery = jquery;
 
-
-export {
-    Isotope,
-    Splide,
-    Micromodal,
-    Email
-}
+export { Isotope, Splide, Micromodal, Email };
